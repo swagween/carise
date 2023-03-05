e42f7628101f47a75f81770468450290b5ec4e8b
zhuzhile08 <zhuzhile08@gmail.com>
2023-03-05 20:20:53 +0100

    link SFML via submodles and add test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,23 @@
-//
-//  main.cpp
-//  carise
-//  05MAR2023
-//
-
-#include <cstdio>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    printf("Hello, world!\n");
+    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
-
-
